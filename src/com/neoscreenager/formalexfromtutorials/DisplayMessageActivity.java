15c87b12d4a5e9d6bf288a7249a984ea105af2e6
neoscreenager <neoscreenager@gmail.com>
2015-06-13 01:18:45 +0530

    added code for new activity and calling it through intent. code updated from android studio.

---
+++ src/com/neoscreenager/formalexfromtutorials/DisplayMessageActivity.java
@@ -0,0 +1,17 @@
+package com.neoscreenager.formalexfromtutorials;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by neo on 6/13/15.
+ */
+public class DisplayMessageActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_display_message);
+        Intent inent = getIntent();
+    }
+}
